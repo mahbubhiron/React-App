d61b2f94e7bf77fbcdf8834a5dc8aee316da740f
Md. Mahbub hiron <mhalom19@gmail.com>
2022-02-23 01:47:01 +0600

    work on 22/feb/2022

--- core-concept/src/App.js
+++ core-concept/src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const students = [{
+    name: 'Mahbub Alom',
+    address: 'mirpur-2',
+    phn: '01914679736',
+    email: 'mhalom19@gmail.com',
+    job: {
+      name: 'singer',
+      address: 'stage-performer'
+    }
+  },
+  {
+    name: 'kkkkkkk',
+    address: 'mirpur-2',
+    phn: '019146797',
+    email: 'mhalom19@gmail.com',
+    job: {
+      name: 'singer',
+      address: 'stage-performer'
+    }
+  },
+  {
+    name: 'lllll',
+    address: 'mirpur-2',
+    phn: '01914679736',
+    email: 'mhalom19@gmail.com',
+    job: {
+      name: 'singer',
+      address: 'stage-performer'
+    }
+  },
+  {
+    name: 'uuuuuu',
+    address: 'mirpur-2',
+    phn: '019136',
+    email: 'mhalom19@gmail.com',
+    job: {
+      name: 'singer',
+      address: 'stage-performer'
+    }
+  },
+  {
+    name: 'jjjjjjj',
+    address: 'mirpur-2',
+    phn: '01914',
+    email: 'mhalom19@gmail.com',
+    job: {
+      name: 'singer',
+      address: 'stage-performer'
+    }
+  }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter></Counter>
+      <Users></Users>
+      <div className="card-container">
+        {
+          students.map(student => <Students students={student}></Students>)
+        }
+      </div>
     </div>
   );
+
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+// dynamic data load from users api
+function Users() {
+  const liStyle = {
+    listStyle: 'none',
+    backgroundColor: 'black',
+    color: 'white',
+    padding: '5px',
+    margin: '3px',
+    width: '200px',
+    textAlign: 'center',
+  }
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic Users : {users.length}</h3>
+      {
+        users.map(user => <li style={liStyle}>{user.name}</li>)
+      }
+    </div>
+  )
+}
+function Students(props) {
+  // const {name, address} =props.users;
+  const details = `${props.students.name}
+  ${props.students.address}
+  ${props.students.phn}
+  ${props.students.email}
+  ${props.students.job.name}
+  ${props.students.job.address}
+  `;
+  return (
+    <div class="card">
+      <div class="card-header">
+        <h2>Name : {props.students.name}</h2>
+      </div>
+      <div class="card-body">
+        <img src="https://images.unsplash.com/photo-1645341777045-8f18b88e2712?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="card-image" alt="logo" />
+        <p>Details : <pre>{details}</pre></p>
+      </div>
+      <div class="card-footer">
+        <p>Footer.......!</p>
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ recap/src/App.js
@@ -0,0 +1,61 @@
+
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [naykos, setNykos] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setNykos(data));
+  }, [])
+  const n = naykos.slice(0,5);
+  return (
+    <div className="App">
+      <MovieCount></MovieCount>
+      {
+        
+        n.map(nk => <Nyok name={nk.name} key={nk.id} email={nk.email}></Nyok>)
+      }
+    </div>
+  );
+}
+function Nyok(props) {
+  const liStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    width: '500px',
+    margin: '5px',
+    padding: '10px'
+  }
+  return (
+    <div>
+      <li style={liStyle}>{props.name} || {props.email}</li>
+    </div>
+  )
+}
+
+function MovieHandaler(props) {
+  return (
+    <div>
+      <h2>My Movie : {props.count} </h2>
+    </div>
+  )
+}
+
+function MovieCount() {
+  const [counter, setCounter] = useState(0);
+  const countHandaler = () => setCounter(counter + 1);
+  return (
+    <div>
+      <button onClick={countHandaler}>Add Movie..!</button>
+      <h3>Total Movie : {counter}</h3>
+      <MovieHandaler count={counter}></MovieHandaler>
+      <MovieHandaler count={counter + 2}></MovieHandaler>
+      <MovieHandaler count={counter + 5}></MovieHandaler>
+    </div>
+  )
+}
+
+
+export default App;
